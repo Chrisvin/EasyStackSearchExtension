3fc69286db2db5776c56969aefeba02e53273728
Jem <chrisvin1997@gmail.com>
2020-06-30 04:57:53 +0530

    Add js handling for options page

---
+++ extension/options/js/options.js
@@ -0,0 +1,29 @@
+
+
+function setupOptions() {
+
+    chrome.storage.sync.get({
+        shouldOpenInSameTab: true
+    }, function(items) {
+        if (items.shouldOpenInSameTab) {
+            document.getElementById('same_tab_result').checked = true;
+        } else {
+            document.getElementById('new_tab_result').checked = true;
+        }
+    });
+
+    $("#save_options").click(function() {
+        chrome.storage.sync.set({
+            shouldOpenInSameTab: $("#same_tab_result").is(":checked")
+        }, function(items) {
+            $(".alert").addClass("show");
+            setTimeout(function() {
+                $("#save_options").blur();
+                $(".alert").removeClass("show");
+            }, 750);
+        });
+    });;
+
+}
+
+setupOptions();
