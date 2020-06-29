c139fbbf3759130b797344ff88c6c8cc6eefed00
Jem <chrisvin1997@gmail.com>
2020-06-29 23:40:43 +0530

    Check permissions & options before opening search result

--- extension/background.js
+++ extension/background.js
@@ -1,5 +1,17 @@
 'use strict';
 
+function checkOptionalPermissions(permissionList, onPermissionsAvailable, onPermissionsUnavailable) {
+  chrome.permissions.contains({
+      permissions: permissionList
+  }, function(result) {
+      if (result) {
+          onPermissionsAvailable();
+      } else {
+          onPermissionsUnavailable();
+      }
+  });
+}
+
 // Open stackoverflow search in same tab when input is entered
 chrome.omnibox.onInputEntered.addListener(
   function(text) {
@@ -11,9 +23,28 @@ chrome.omnibox.onInputEntered.addListener(
     var finalURL = prefix + seURL + postfix + encodeURIComponent(text);
     
     // TODO: Open url in current/new tab based on user settings.
-    chrome.tabs.getSelected(null, function(tab) {
-      chrome.tabs.update(tab.id, {url: finalURL});
-    });  
+
+    checkOptionalPermissions(
+      ['storage'],
+      function onPermissionsAvailable() {
+        chrome.storage.sync.get({
+          shouldOpenInSameTab: true
+        }, function(items) {
+          if (items.shouldOpenInSameTab) {
+            chrome.tabs.getSelected(null, function(tab) {
+              chrome.tabs.update(tab.id, {url: finalURL});
+            });
+          } else {
+            chrome.tabs.create({ url: finalURL });
+          }
+        });
+      },
+      function onPermissionsUnavailable() {
+        // chrome.tabs.getSelected(null, function(tab) {
+          // chrome.tabs.update(tab.id, {url: finalURL});
+        // });
+        chrome.tabs.create({ url: finalURL });
+    });
 });
 
 // Suggest specific questions as text is being typed.
