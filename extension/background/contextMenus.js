05cfaa22c5dcc84341128e89193272fafd8b15ef
Jem <chrisvin1997@gmail.com>
2020-07-02 05:56:13 +0530

    Add default filter handling in contextMenu
    Will be added to omnibox once all issues have been fixed.

--- extension/background/contextMenus.js
+++ extension/background/contextMenus.js
@@ -1,6 +1,40 @@
 
 'use strict';
 
+function getFilterString(
+    questionsWithCodeFilter,
+    communityWikisFilter,
+    excludeDuplicateQuestionsFilter,
+    answerTypeFilter,
+    customFilter
+    ) {
+        var filterString = customFilter+" ";
+        if (questionsWithCodeFilter) {
+            filterString += "hascode:true ";
+        }
+        if (communityWikisFilter) {
+            filterString += "wiki:true ";
+        }
+        if (excludeDuplicateQuestionsFilter) {
+            filterString += "duplicate:false ";
+        }
+        switch (answerTypeFilter) {
+            //TODO: Fix me, I don't work for some reason. :(
+            case 1:
+                filterString += "answers:1 ";
+                break;
+            case 2:
+                filterString += "hasaccepted:true";
+                break;
+            case 3:
+                filterString += "isanswered:true";
+                break;
+            default:
+                break;
+        }
+        return filterString;
+}
+
 chrome.runtime.onInstalled.addListener(function() {
     chrome.contextMenus.create({
         id: "stack_overflow_search_context_id",
@@ -11,6 +45,21 @@ chrome.runtime.onInstalled.addListener(function() {
 });
 
 chrome.contextMenus.onClicked.addListener(function(item, tab) {
-    let searchUrl = "https://www.stackoverflow.com/search?q="+item.selectionText;
-    chrome.tabs.create({url: searchUrl, index: tab.index + 1});
+    chrome.storage.sync.get({
+        questionsWithCodeFilter: false,
+        communityWikisFilter: false,
+        excludeDuplicateQuestionsFilter: false,
+        answerTypeFilter: 0,
+        customFilter: ""
+    }, function(items) {
+        let filterString = getFilterString(
+            items.questionsWithCodeFilter, 
+            items.communityWikisFilter, 
+            items.excludeDuplicateQuestionsFilter, 
+            items.answerTypeFilter, 
+            items.customFilter);
+        let searchText = filterString + item.selectionText;
+        let searchUrl = "https://www.stackoverflow.com/search?q="+encodeURIComponent(searchText)
+        chrome.tabs.create({url: searchUrl, index: tab.index + 1});
+    });
 });
